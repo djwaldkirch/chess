a87b296d5bef4ec8dd0f93806258cbbd82da39d1
Dan Waldkirch <djwaldkirch@gmail.com>
2018-12-10 17:45:56 -0800

    Added pseudocode for game

--- chess.rb
+++ chess.rb
@@ -2,14 +2,38 @@ require_relative 'lib/board.rb'
 require_relative 'lib/pieces.rb'
 require_relative 'lib/helper.rb'
 
+#set up board
 c = Board.new
 c.set_up_board
 c.display
-
+#make move
+  #ask what piece you want to move
 puts "White to move."
 puts "Select a piece:"
 choice = gets.chomp.to_sym
 selected_piece = c.pieces.find { |piece| piece.position == choice }
+
+    #check if that is a valid square
+    #check if there is a piece there
+  #select that piece
+  #get possible moves for that piece
+  #ask where you want to move to
+  #see if that is in the list of possible moves
+  #make the move!
+  #check if you have captured someone
+    #if so, remove that object from the board
+  #check if you have promoted
+  #check if you have delivered checkmate
+    #if so, end the game
+  #check if you have delivered check
+  #check for stalemate
+    #if so, end the game
+  #switch to other player's turn
+
+
+
+
+
 
 puts "Where do you want it to go?"
 destination = gets.chomp.to_sym
